275bfa7c509475954c2ec0325d582d43814f5fa9
athamour1 <thanosmour.tk@gmail.com>
2021-04-10 20:07:00 +0300

    Initial Commit 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,57 @@
-#include <Arduino.h>
-#include <SPI.h>
-#include <Ethernet.h>
 
-byte mac[] = {
-  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
-};
+#include <Arduino.h>
+#include <SimpleDHT.h>
+#include <Ethernet.h>
+#include <SPI.h>
+#include <UbidotsEthernet.h>
+
+char const * TOKEN = "token_here"; // Assign your Ubidots TOKEN
+char const * VARIABLE_LABEL_1 = "temperature"; // Assign the unique variable label to send the data
+char const * VARIABLE_LABEL_2 = "humidity"; // Assign the unique variable label to send the data
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+Ubidots client(TOKEN);
+
+int pinDHT11 = 2;
+SimpleDHT11 dht11(pinDHT11);
 
 void setup() {
-
   Serial.begin(9600);
-  while (!Serial) {
-    ;
+  Serial.print(F("Starting ethernet..."));
+  if (!Ethernet.begin(mac)) {
+    Serial.println(F("failed"));
+  } else {
+    Serial.println(Ethernet.localIP());
   }
-
-  Serial.println("Initialize Ethernet with DHCP:");
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    } else if (Ethernet.linkStatus() == LinkOFF) {
-      Serial.println("Ethernet cable is not connected.");
-    }
-    while (true) {
-      delay(1);
-    }
-  }
-  // print your local IP address:
-  Serial.print("My IP address: ");
-  Serial.println(Ethernet.localIP());
+  delay(2000);
+  Serial.println(F("Ready"));
 }
 
 void loop() {
-  switch (Ethernet.maintain()) {
-    case 1:
-      //renewed fail
-      Serial.println("Error: renewed fail");
-      break;
 
-    case 2:
-      //renewed success
-      Serial.println("Renewed success");
-      //print your local IP address:
-      Serial.print("My IP address: ");
-      Serial.println(Ethernet.localIP());
-      break;
+  Ethernet.maintain();
 
-    case 3:
-      //rebind fail
-      Serial.println("Error: rebind fail");
-      break;
-
-    case 4:
-      //rebind success
-      Serial.println("Rebind success");
-      //print your local IP address:
-      Serial.print("My IP address: ");
-      Serial.println(Ethernet.localIP());
-      break;
-
-    default:
-      //nothing happened
-      break;
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(1000);
+    return;
   }
+
+  Serial.print("Sample OK: ");
+  Serial.print((int)temperature); Serial.print(" *C, ");
+  Serial.print((int)humidity); Serial.println(" H");
+
+  /* Sensors readings */
+  float value_1 = temperature;
+  float value_2 = humidity;
+  /* Sending values to Ubidots */
+  client.add(VARIABLE_LABEL_1, value_1);
+  client.add(VARIABLE_LABEL_2, value_2);
+  client.sendAll();
+
+  delay(5000);
 }
